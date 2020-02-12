2992d284f48c88dd95c0ab62a497b7e477e1f23e
David A <david1906@outlook.com>
2020-02-11 21:16:26 -0600

    Updates

---
+++ precache-manifest.3ced35bdcdee0bf673c2bc0014019f99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77cf4828066a66c730e6",
+    "url": "/dadJokes/static/js/runtime~main.77cf4828.js"
+  },
+  {
+    "revision": "2c5be4b320afb51c66d4",
+    "url": "/dadJokes/static/js/main.2c5be4b3.chunk.js"
+  },
+  {
+    "revision": "43a33ea9b5862b0054c4",
+    "url": "/dadJokes/static/js/1.43a33ea9.chunk.js"
+  },
+  {
+    "revision": "2c5be4b320afb51c66d4",
+    "url": "/dadJokes/static/css/main.1d27d8a6.chunk.css"
+  },
+  {
+    "revision": "84cca0d3296ceca338be98c66a78ff18",
+    "url": "/dadJokes/index.html"
+  }
+];
