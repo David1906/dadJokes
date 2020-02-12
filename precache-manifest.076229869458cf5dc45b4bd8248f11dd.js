a2218f0717d95555fc6b8dd0fba0d2f713ff7ec9
David A <david1906@outlook.com>
2020-02-12 00:01:24 -0600

    Updates

---
+++ precache-manifest.076229869458cf5dc45b4bd8248f11dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b427bbb4c15ecc453a2db9aa6dcb93c",
+    "url": "/dadJokes/index.html"
+  },
+  {
+    "revision": "a8be243eb7e8d2cd7196",
+    "url": "/dadJokes/static/css/main.3fa0cea2.chunk.css"
+  },
+  {
+    "revision": "7929bcb6e30bc1463b92",
+    "url": "/dadJokes/static/js/2.681d3bc9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dadJokes/static/js/2.681d3bc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8be243eb7e8d2cd7196",
+    "url": "/dadJokes/static/js/main.4b26bdbd.chunk.js"
+  },
+  {
+    "revision": "6dfb65dd905de058a61c",
+    "url": "/dadJokes/static/js/runtime-main.01b4f8f5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dadJokes/precache-manifest.3ced35bdcdee0bf673c2bc0014019f99.js"
+  "/dadJokes/precache-manifest.076229869458cf5dc45b4bd8248f11dd.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dadJokes/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dadJokes/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
